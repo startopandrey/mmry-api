99e493431288cdf4d2f0321a91f17d8829c84436
Andrey Ilyukhin <startopandrey@gmail.com>
2024-04-13 10:43:59 +0200

    set port env

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -25,6 +26,7 @@ async function bootstrap() {
     },
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  console.log('port', process.env.PORT);
+  await app.listen(process.env.PORT, () => '0.0.0.0');
 }
 bootstrap();
